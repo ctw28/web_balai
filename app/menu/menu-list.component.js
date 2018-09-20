9310aebc515ad1b0b9dab1de16d1b2de1bbfc06b
localhouse <mbandrigo@gmail.com>
2018-09-20 16:53:37 +0800

    angular template

--- app/menu/menu-list.component.js
+++ app/menu/menu-list.component.js
@@ -2,13 +2,7 @@
 
 angular.module('menuList').
         component('menuList',{
-            template :'<ul>' +
-            '<li ng-repeat="menu in $ctrl.menuData">{{menu.nama_menu}}'+
-                '<ul>'+
-                    '<li ng-repeat="submenu in menu.sub_menu">{{submenu.nama_sub}}</li>'+
-                '</ul>'+
-            '</li>'+
-        '</ul>',
+            templateUrl :'app/menu/menu-list.template.html',
             controller : ['$http',function menuController($http){
                 var self = this;
                 $http.get('http://localhost/web_balai/data/rest_menu')
